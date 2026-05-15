23024f7cc9313f3830bb5e19a2b9c995e5beed7a
Yelena <y.puentes@uniandes.edu.co>
2026-05-15 10:15:15 -0500

    feat(items): add Item entity with ItemType enum

---
+++ src/modules/items/entities/item.entity.ts
@@ -0,0 +1,34 @@
+import {
+  Column, CreateDateColumn, Entity,
+  PrimaryGeneratedColumn, UpdateDateColumn,
+} from 'typeorm';
+
+export enum ItemType {
+  BOOK = 'book',
+  MAGAZINE = 'magazine',
+  EQUIPMENT = 'equipment',
+}
+
+@Entity('items')
+export class Item {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column({ length: 32, unique: true })
+  code: string;
+
+  @Column({ length: 255 })
+  title: string;
+
+  @Column({ type: 'enum', enum: ItemType })
+  type: ItemType;
+
+  @Column({ default: true })
+  isActive: boolean;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+}
